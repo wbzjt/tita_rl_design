3c35f09c6b507f5186b37d522450edab2c3d8eee
wbzjt <3319963854@qq.com>
2025-06-20 11:22:59 +0800

    修改初始角度 增加姿态惩罚 减少collision 减少tracking_Vel 鼓励触地 减小survival微调参数 3000轮效果好转

--- configs/tita_flat_config.py
+++ configs/tita_flat_config.py
@@ -32,8 +32,8 @@ class TitaFlatCfg(TitaRoughCfg):
             "joint_right_leg_1": 0.0,
             "joint_left_leg_2": 0.8,
             "joint_right_leg_2": 0.8,
-            "joint_left_leg_3": -1.5,
-            "joint_right_leg_3": -1.5,
+            "joint_left_leg_3": -1.3,
+            "joint_right_leg_3": -1.3,
             "joint_left_leg_4": 0.0,
             "joint_right_leg_4": 0.0,
         }   
@@ -88,33 +88,33 @@ class TitaFlatCfg(TitaRoughCfg):
             # base class
             lin_vel_z = 0.0 # off 不奖励z方向移动
             ang_vel_xy = 0.0 # off 忽略XY面角速度
-            orientation = -5.0 # 很重要，不加的话会导致存活时间下降 强烈惩罚姿态不稳定以对抗跌倒或旋转失衡
+            orientation = -6.0 # 很重要，不加的话会导致存活时间下降 强烈惩罚姿态不稳定以对抗跌倒或旋转失衡
             base_height = -20.0 # 强烈惩罚base高度偏离目标高度
             torques = -2.5e-05  #惩罚使用较大电机扭矩，促进能量效率。
             dof_vel = 0.0 # off # 关节速度不计入奖励。
-            dof_acc = -2.5e-07 # 轻微惩罚关节加速度变化，有利于平滑控制。
+            dof_acc = -2.5e-06 # 轻微惩罚关节加速度变化，有利于平滑控制。
             action_rate = -0.01 # 轻微惩罚动作变化，促进平稳运动。
-            collision = -10.0 # 强烈惩罚碰撞，防止机器人与地面或其他物体发生碰撞。
+            collision = -8.0 # 强烈惩罚碰撞，防止机器人与地面或其他物体发生碰撞。
             termination = 0.0 # off 终止奖励不计入，可能会导致过早终止。
             dof_pos_limits = -2.0 # 轻微惩罚关节位置超出限制，促进关节在安全范围内运动。
             torque_limits = 0.0 # off不惩罚扭矩限制，可能会导致过度使用电机。
-            tracking_lin_vel = 10.0 # 跟踪线速度奖励，促进机器人沿目标速度移动。
-            tracking_ang_vel = 5.0 # off 角速度跟踪奖励，促进机器人沿目标角速度旋转。
+            tracking_lin_vel = 5.0 # 跟踪线速度奖励，促进机器人沿目标速度移动。
+            tracking_ang_vel = 2.0 # off 角速度跟踪奖励，促进机器人沿目标角速度旋转。
             feet_air_time = 0.0 # off 脚离地时间不奖励。
-            no_fly = 1.0 # off 	鼓励双轮总有一只触地（不“飞起”）。
+            no_fly = 1.5 # off 	鼓励双轮总有一只触地（不“飞起”）。
             unbalance_feet_air_time = 0.0 # off
             unbalance_feet_height = 0.0 # off
             feet_stumble = 0.0 # off	暂不计入脚步不平衡奖励。
             stand_still = -1.0 # 不奖励静止状态，可能导致机器人不动。
             feet_contact_forces = 0.0 # off 接触力未计入奖励。
             feet_distance = -100 # -100 	脚之间的水平距离不合适时惩罚很大。
-            survival = 0.3 # 生存奖励，鼓励机器人尽可能长时间存活。
+            survival = 0.1 # 生存奖励，鼓励机器人尽可能长时间存活。
             # new added
-            wheel_adjustment = 1.0 # 1.0 off 轮子调整奖励，鼓励轮子在地面上保持适当位置。
+            wheel_adjustment = 1.1 # 1.0 off 轮子调整奖励，鼓励轮子在地面上保持适当位置。
             inclination = 0.0 # off 倾斜奖励，可能会导致机器人在倾斜地面上不稳定。
-            leg_symmetry = 12.0 # 10.0 鼓励腿部对称性，促进平衡和稳定。
+            leg_symmetry = 11.0 # 10.0 鼓励腿部对称性，促进平衡和稳定。
 
-        base_height_target = 0.281 # 基础高度目标，机器人应尽量保持在此高度。
+        base_height_target = 0.261 # 基础高度目标，机器人应尽量保持在此高度。
         soft_dof_pos_limit = 0.95  # percentage of urdf limits, values above this limit are penalized
         soft_dof_vel_limit = 1.0
         min_feet_distance = 0.44
@@ -133,4 +133,4 @@ class TitaFlatCfgPPO(TitaRoughCfgPPO):
 
     class runner(TitaRoughCfgPPO.runner):
         experiment_name = 'tita_flat'
-        max_iterations = 80000
+        max_iterations = 3000
